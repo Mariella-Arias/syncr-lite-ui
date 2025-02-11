5171a1652c541981302e4ef0e08c6227ad8c5a3e
Mariella-Arias <ariasmariella.brm@gmail.com>
2025-02-11 14:01:30 -0600

    Move sign up form submission to handle inside sign up page

--- src/pages/auth/SignupPage.tsx
+++ src/pages/auth/SignupPage.tsx
@@ -1,15 +1,35 @@
 import { Link } from 'react-router';
+import { AxiosError } from 'axios';
 
 import logo from '../../assets/logo.png';
-import SignupForm from '../../features/auth/components/SignupForm';
+import SignupForm, {
+  IUserProfile,
+} from '../../features/auth/components/SignupForm';
+import api, { handleApiError } from '../../services/api';
 
 const SignupPage = () => {
+  const handleSignup = async (values: IUserProfile) => {
+    try {
+      await api.post('auth/users/', {
+        email: values.email,
+        first_name: values.firstName,
+        last_name: values.lastName,
+        password: values.password,
+        re_password: values.rePassword,
+      });
+    } catch (err: unknown) {
+      if (err instanceof AxiosError) {
+        throw handleApiError(err);
+      }
+    }
+  };
+
   return (
     <div className="flex justify-center">
       <div className="translate-y-1/8 border-input-border border-1 shadow-md w-[400px] flex flex-col px-12 py-10 items-center gap-3 rounded-[10px]">
         <img src={logo} className="mb-7 p-3" />
         <p className="font-nunito text-3xl font-bold">Create your account</p>
-        <SignupForm />
+        <SignupForm handleSubmit={handleSignup} />
         <p>
           Already have an account?
           <Link to="/login">
