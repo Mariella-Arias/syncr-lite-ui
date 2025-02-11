358ac3882c51425ba7ec99824cf47151c7d3ae5b
Mariella-Arias <ariasmariella.brm@gmail.com>
2025-02-11 14:05:56 -0600

    Create account activation page

--- src/pages/auth/ActivatePage.tsx
+++ src/pages/auth/ActivatePage.tsx
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+import { useParams, useNavigate } from 'react-router';
+
 import logo from '../../assets/logo.png';
 import Button from '../../components/common/Button';
+import { useAuthApi } from '../../features/auth/hooks/useAuthApi';
 
 const ActivatePage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { uid, token } = useParams();
+  const navigate = useNavigate();
+  const { activate } = useAuthApi();
+
   return (
     <div className="flex justify-center">
       <div className="translate-y-1/8 border-input-border border-1 shadow-md w-[400px] flex flex-col px-12 py-10 items-center gap-3 rounded-[10px]">
@@ -11,8 +20,28 @@ const ActivatePage = () => {
           Your account is ready to be activated. Click the button below to
           complete the verification process.
         </p>
-        <Button size="medium" className="w-full">
-          <span className="text-lg">Verify</span>
+        <Button
+          size="medium"
+          className="w-full"
+          disabled={isLoading}
+          onClick={async () => {
+            setIsLoading(true);
+            try {
+              await activate({ uid, token });
+              navigate('/login');
+            } catch {
+              console.log('ERROR VERIFYING');
+            }
+            setIsLoading(false);
+          }}
+        >
+          <div className="flex items-center justify-center h-full w-full">
+            {isLoading ? (
+              <span className="border-t-2 border-solid  w-7 h-7 rounded-full animate-spin"></span>
+            ) : (
+              <span className="text-lg">Verify</span>
+            )}
+          </div>
         </Button>
       </div>
     </div>
