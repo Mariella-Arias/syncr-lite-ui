0afaadf98958b56db49d994057eb736fdf73a572
Mariella-Arias <ariasmariella.brm@gmail.com>
2025-02-12 12:57:30 -0600

    Replace svg icon with styled div

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -23,20 +23,7 @@ const UserIcon = () => {
 
 const ChevronIcon = () => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="size-4"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="m19.5 8.25-7.5 7.5-7.5-7.5"
-      />
-    </svg>
+    <div className="border-t-2 border-r-2 w-2 h-2 rotate-135 border-solid "></div>
   );
 };
 
