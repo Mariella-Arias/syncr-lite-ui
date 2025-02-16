b32048511abb81f26e60780cab60956d1a5e08cf
Mariella-Arias <ariasmariella.brm@gmail.com>
2025-02-15 20:23:14 -0600

    Update DeleteAccountModal component to work with reusable Modal component

--- src/features/auth/components/DeleteAccountModal.tsx
+++ src/features/auth/components/DeleteAccountModal.tsx
@@ -1,37 +1,31 @@
 import { AxiosError } from 'axios';
 
 import { handleApiError } from '../../../services/api';
-import Modal from '../../../components/overlays/Modal';
 import DeleteAccountForm from './DeleteAccountForm';
 import { useAuthApi } from '../hooks/useAuthApi';
-import { useAuth } from '../../../context/AuthContext';
 
 const DeleteAccountModal = () => {
-  const { checkAuthStatus } = useAuth();
   const { deleteAccount } = useAuthApi();
 
   return (
-    <Modal>
-      <div className="flex flex-col gap-2">
-        <p className="font-nunito text-2xl font-bold mt-2 self-center">
-          Delete Account
-        </p>
-        <p>Are you sure you want to delete your account?</p>
-        <p>Enter password to confirm:</p>
-        <DeleteAccountForm
-          handleSubmit={async (data: Record<string, string>) => {
-            try {
-              await deleteAccount({ current_password: data.password });
-              await checkAuthStatus();
-            } catch (err: unknown) {
-              if (err instanceof AxiosError) {
-                throw handleApiError(err);
-              }
+    <div className="flex flex-col gap-2">
+      <p className="font-nunito text-2xl font-bold mt-2 self-center">
+        Delete Account
+      </p>
+      <p>Are you sure you want to delete your account?</p>
+      <p>Enter password to confirm:</p>
+      <DeleteAccountForm
+        handleSubmit={async (data: Record<string, string>) => {
+          try {
+            await deleteAccount({ current_password: data.password });
+          } catch (err: unknown) {
+            if (err instanceof AxiosError) {
+              throw handleApiError(err);
             }
-          }}
-        />
-      </div>
-    </Modal>
+          }
+        }}
+      />
+    </div>
   );
 };
 
