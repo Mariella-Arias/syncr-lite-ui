703703b0492c954906ad195ba1e6f784ead3fb23
Mariella-Arias <ariasmariella.brm@gmail.com>
2025-03-05 13:20:21 -0600

    feat (workouts): create new exercise form component

---
+++ src/features/workouts/components/CreateExerciseForm.tsx
@@ -0,0 +1,112 @@
+import { Formik, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+import Button from '../../../components/common/Button';
+
+export interface INewExerciseData {
+  value: string;
+  label: string;
+  user?: number;
+  tracking_param: string;
+}
+
+export interface INewExercise {
+  name: string;
+  parameter: string;
+}
+
+const CreateExerciseForm = ({
+  initialName,
+  createExercise,
+}: {
+  initialName: string;
+  createExercise: (data: INewExercise) => Promise<void>;
+}) => {
+  const exerciseSchema = Yup.object().shape({
+    name: Yup.string()
+      .required('Exercise name is required')
+      .matches(/^[a-zA-Z0-9\s]+$/, 'Invalid special character')
+      .trim(),
+
+    parameter: Yup.string()
+      .required('Required')
+      .oneOf(['reps', 'duration'], 'Required'),
+  });
+
+  return (
+    <div className="flex flex-col gap-2 mt-1">
+      <p className="text-lg text-body-text font-semibold">
+        Quick Add New Exercise
+      </p>
+      <Formik
+        initialValues={{ name: initialName, parameter: 'reps' }}
+        validationSchema={exerciseSchema}
+        onSubmit={async (values, { resetForm }) => {
+          try {
+            await createExercise(values);
+          } catch (err) {
+            console.log(err);
+          } finally {
+            resetForm();
+          }
+        }}
+      >
+        {({ isSubmitting, handleSubmit }) => {
+          const handleClick = async () => {
+            handleSubmit();
+          };
+
+          return (
+            <div className="flex flex-col gap-2">
+              <div>
+                <Field
+                  name="name"
+                  autoComplete="off"
+                  placeholder="Exercise Name"
+                  className="border-input-border border-1 rounded-[10px] py-2 px-3 text-lg"
+                />
+                <ErrorMessage
+                  name="name"
+                  component="div"
+                  className="text-red-550 text-sm"
+                />
+              </div>
+              <div>
+                <Field
+                  name="parameter"
+                  as="select"
+                  placeholder="Select Tracking Unit"
+                  className="border-input-border border-1 rounded-[10px] py-2 px-3 text-lg"
+                >
+                  <option value="reps">Reps</option>
+                  <option value="duration">Duration</option>
+                </Field>
+                <ErrorMessage
+                  name="parameter"
+                  component="div"
+                  className="text-red-550 text-sm"
+                />
+              </div>
+              <Button
+                onClick={handleClick}
+                size="medium"
+                type="button"
+                className=""
+              >
+                <div className="flex items-center justify-center h-full w-full">
+                  {isSubmitting ? (
+                    <span className="border-t-2 border-solid  w-7 h-7 rounded-full animate-spin"></span>
+                  ) : (
+                    <span className="text-lg">Create Exercise</span>
+                  )}
+                </div>
+              </Button>
+            </div>
+          );
+        }}
+      </Formik>
+    </div>
+  );
+};
+
+export default CreateExerciseForm;
--- src/features/workouts/components/ExerciseSearch.tsx
+++ src/features/workouts/components/ExerciseSearch.tsx
@@ -4,11 +4,18 @@ import {
   useEffect,
   ChangeEvent,
   MouseEvent as ReactMouseEvent,
+  ReactNode,
 } from 'react';
 import { FieldInputProps } from 'formik';
+import { useSelector } from 'react-redux';
 
 import { IExercise } from './CreateWorkoutModal';
 import Button from '../../../components/common/Button';
+import CreateExerciseForm, {
+  INewExerciseData,
+  INewExercise,
+} from './CreateExerciseForm';
+import { auth } from '../../auth/authSlice';
 
 interface ExerciseAutocompleteProps {
   field: FieldInputProps<string>;
@@ -26,6 +33,7 @@ interface ExerciseAutocompleteProps {
       shouldValidate?: boolean
     ) => Promise<void>;
   };
+  createExercise: (data: INewExerciseData) => Promise<void>;
 }
 
 const SearchIcon = () => {
@@ -52,16 +60,35 @@ const ExerciseSearch = ({
   options,
   onDeleteExercise,
   form: { setFieldValue, setFieldTouched },
+  createExercise,
   ...props
 }: ExerciseAutocompleteProps) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isSearchIcon, setIsSearchIcon] = useState(true);
+  const [exerciseForm, setExerciseForm] = useState<ReactNode>(null);
   const { name, value, onChange } = field;
   const fieldRef = useRef<HTMLDivElement>(null);
+  const { user } = useSelector(auth);
 
   const filteredExercises = options.filter((option) =>
     option.value.includes(value.toLowerCase())
   );
+
+  const handleCreateExercise = async ({ name, parameter }: INewExercise) => {
+    const titleCaseName = name
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ');
+
+    const data = {
+      value: name.toLowerCase(),
+      label: titleCaseName,
+      user: user?.id,
+      tracking_param: parameter,
+    };
+
+    await createExercise(data);
+  };
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -79,7 +106,6 @@ const ExerciseSearch = ({
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
-
   return (
     <div
       className={`relative transition-spacing duration-250 ease-in-out ${
@@ -116,66 +142,89 @@ const ExerciseSearch = ({
         />
         <Button
           size="medium"
-          disabled
+          disabled={value.length === 0 || filteredExercises.length > 0}
           className={`z-20 ${isDropdownOpen ? 'inline' : 'hidden'}`}
+          type="button"
+          onClick={() => {
+            setExerciseForm(
+              <CreateExerciseForm
+                initialName={value}
+                createExercise={async ({ name, parameter }: INewExercise) => {
+                  await handleCreateExercise({ name, parameter });
+                  setExerciseForm(null);
+                }}
+              />
+            );
+          }}
         >
           Add
         </Button>
       </div>
       {isDropdownOpen && (
-        <div className="absolute transition-transform duration-200 ease-in-out bg-white rounded-[10px] shadow-md top-0 pt-17 left-0 right-0 -mx-3 -translate-y-3 z-30 border-1 border-input-border p-3 flex flex-col cursor-pointer max-h-63">
+        <div className="absolute transition-transform duration-200 ease-in-out bg-white rounded-[10px] shadow-md top-0 pt-17 left-0 right-0 -mx-3 -translate-y-3 z-30 border-1 border-input-border p-3 flex flex-col cursor-pointer max-h-67">
           <div className="flex-1 overflow-y-auto no-scrollbar">
-            {filteredExercises.map((option) => (
-              <div
-                key={option.value}
-                onClick={() => {
-                  // Set exercise value and trigger validation on its field
-                  setFieldValue(name, option.label);
-                  setFieldTouched(name, true, true);
+            {filteredExercises.length ? (
+              filteredExercises.map((option) => (
+                <div
+                  key={option.value}
+                  onClick={() => {
+                    // Set exercise value and trigger validation on its field
+                    setFieldValue(name, option.label);
+                    setFieldTouched(name, true, true);
 
-                  setIsSearchIcon(false);
-                  setIsDropdownOpen(false);
+                    setIsSearchIcon(false);
+                    setIsDropdownOpen(false);
 
-                  // Update Fields-array to include the newly selected exercise's tracking parameter
-                  const pathPieces = name.split('.');
+                    // Update Fields-array to include the newly selected exercise's tracking parameter
+                    const pathPieces = name.split('.');
 
-                  const blockIdx = parseInt(pathPieces[1]);
-                  const exerciseIdx = parseInt(pathPieces[3]);
+                    const blockIdx = parseInt(pathPieces[1]);
+                    const exerciseIdx = parseInt(pathPieces[3]);
 
-                  // Update Fields[]
-                  const fieldsPath = `blocks.${blockIdx}.exercises.${exerciseIdx}.fields`;
-                  const newFields = ['sets', option.tracking_param];
-                  setFieldValue(fieldsPath, newFields);
+                    // Update Fields[]
+                    const fieldsPath = `blocks.${blockIdx}.exercises.${exerciseIdx}.fields`;
+                    const newFields = ['sets', option.tracking_param];
+                    setFieldValue(fieldsPath, newFields);
 
-                  // Update Data{}
-                  const dataPath = `blocks.${blockIdx}.exercises.${exerciseIdx}.data`;
-                  setFieldValue(`${dataPath}.${option.tracking_param}`, '');
+                    // Update Data{}
+                    const dataPath = `blocks.${blockIdx}.exercises.${exerciseIdx}.data`;
+                    setFieldValue(`${dataPath}.${option.tracking_param}`, '');
 
-                  setFieldTouched(fieldsPath, true, true);
-                  setFieldTouched(
-                    `${dataPath}.${option.tracking_param}`,
-                    true,
-                    true
-                  );
-                }}
-                className="flex justify-between items-center font-nunito text-base font-semibold text-body-text p-2 hover:font-extrabold odd:bg-[#F3F2F2] even:bg-white relative"
-              >
-                {option.label}
+                    setFieldTouched(fieldsPath, true, true);
+                    setFieldTouched(
+                      `${dataPath}.${option.tracking_param}`,
+                      true,
+                      true
+                    );
+                  }}
+                  className="flex justify-between items-center font-nunito text-base font-semibold text-body-text p-2 hover:font-extrabold odd:bg-[#F3F2F2] even:bg-white relative"
+                >
+                  {option.label}
 
-                {option.is_editable && (
-                  <button
-                    type="button"
-                    onClick={(e: ReactMouseEvent<HTMLButtonElement>) => {
-                      e.stopPropagation();
-                      onDeleteExercise(option);
-                    }}
-                    className="hover:bg-neutral-200 absolute right-0 top-0 bottom-0 px-3 rounded-[10px]"
-                  >
-                    &times;
-                  </button>
-                )}
+                  {option.is_editable && (
+                    <button
+                      type="button"
+                      onClick={(e: ReactMouseEvent<HTMLButtonElement>) => {
+                        e.stopPropagation();
+                        onDeleteExercise(option);
+                      }}
+                      className="hover:bg-neutral-200 absolute right-0 top-0 bottom-0 px-3 rounded-[10px]"
+                    >
+                      &times;
+                    </button>
+                  )}
+                </div>
+              ))
+            ) : exerciseForm ? (
+              exerciseForm
+            ) : (
+              <div>
+                <p className="text-base italic">Exercise not found</p>
+                <p className="text-base italic">
+                  Click "Add" to create a new exercise
+                </p>
               </div>
-            ))}
+            )}
           </div>
         </div>
       )}
