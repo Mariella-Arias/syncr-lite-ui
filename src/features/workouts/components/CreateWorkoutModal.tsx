c96d9daad12aad76d4e145c4162b78b817cfb7a4
Mariella-Arias <ariasmariella.brm@gmail.com>
2025-03-06 23:29:33 -0600

    refactor: fetch workouts and exercises data from redux slice

--- src/features/workouts/components/CreateWorkoutModal.tsx
+++ src/features/workouts/components/CreateWorkoutModal.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
+import { workouts, setExercises, setWorkouts } from '../workoutsSlice';
 import CreateWorkoutForm from './CreateWorkoutForm';
 import { useWorkoutsApi } from '../hooks/useWorkoutsApi';
 import {
@@ -12,28 +13,20 @@ import {
   IExercise,
   IWorkoutData,
 } from '../types/workouts.types';
-
-const Loader = () => {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="border-t-4 border-sky-450 border-solid w-16 h-16 rounded-full animate-spin"></div>
-    </div>
-  );
-};
+import { AppDispatch } from '../../../app/store';
 
 const CreateWorkoutModal = () => {
-  const { createExercise, getExercises, createWorkout, deleteExercise } =
-    useWorkoutsApi();
-  const [isLoading, setIsLoading] = useState(false);
-  const [exercises, setExercises] = useState<IExercise[]>([]);
-  const hasFetched = useRef(false);
+  const { createExercise, createWorkout, deleteExercise } = useWorkoutsApi();
   const { close: closeSlideInModal } = useSlideInModalContext();
   const { open: openCenteredModal, close: closeCenteredModal } =
     useCenteredModalContext();
+  const { exercises } = useSelector(workouts);
+  const dispatch: AppDispatch = useDispatch();
 
   const handleCreateWorkout = async (values: IWorkoutData) => {
     try {
       await createWorkout(values);
+      await dispatch(setWorkouts());
       // TODO: add success notification
     } catch (err) {
       // TODO: add error notification
@@ -51,7 +44,7 @@ const CreateWorkoutModal = () => {
         onDelete={async () => {
           try {
             await deleteExercise({ id: exercise.id });
-            await fetchExercises();
+            await dispatch(setExercises());
           } catch (err) {
             console.log(err);
             // TODO: handle error
@@ -66,29 +59,11 @@ const CreateWorkoutModal = () => {
   const handleCreateExercise = async (data: INewExerciseData) => {
     try {
       await createExercise(data);
-      await fetchExercises();
+      await dispatch(setExercises());
     } catch (err) {
       console.log(err);
     }
   };
-
-  const fetchExercises = async () => {
-    try {
-      const response = await getExercises();
-      setExercises(response);
-    } catch (error) {
-      console.log('Error fetching exercises', error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    if (hasFetched.current) return;
-
-    fetchExercises();
-    hasFetched.current = true;
-  }, [getExercises]);
 
   return (
     <div className="px-12 py-8 h-full flex flex-col">
@@ -96,16 +71,12 @@ const CreateWorkoutModal = () => {
       <p className="font-nunito text-2xl font-bold mb-6">New Workout</p>
       {/* Scrollable Content Area */}
       <div className="flex-1 overflow-y-auto no-scrollbar">
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <CreateWorkoutForm
-            handleSubmit={handleCreateWorkout}
-            deleteExercise={handleDeleteExercise}
-            createExercise={handleCreateExercise}
-            options={exercises}
-          />
-        )}
+        <CreateWorkoutForm
+          handleSubmit={handleCreateWorkout}
+          deleteExercise={handleDeleteExercise}
+          createExercise={handleCreateExercise}
+          options={exercises}
+        />
       </div>
     </div>
   );
