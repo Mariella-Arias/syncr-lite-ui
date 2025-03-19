efd85863947e34b46088dc3036476a65fb3ef440
Mariella-Arias <ariasmariella.brm@gmail.com>
2025-03-18 21:44:44 -0500

    ActivityPage component placeholder

---
+++ src/pages/activity/ActivityPage.tsx
@@ -0,0 +1,9 @@
+const ActivityPage = () => {
+  return (
+    <div className="flex justify-center">
+      <p className="text-2xl">ACTIVITY PAGE</p>
+    </div>
+  );
+};
+
+export default ActivityPage;
