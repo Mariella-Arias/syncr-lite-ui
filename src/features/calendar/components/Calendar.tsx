5c463ff0d937d9c0479cfa7f2318bb4de2dabbee
Mariella-Arias <ariasmariella.brm@gmail.com>
2025-03-19 19:54:49 -0500

    [WIP] Calendar component placeholder

---
+++ src/features/calendar/components/Calendar.tsx
@@ -0,0 +1,9 @@
+const Calendar = () => {
+  return (
+    <div className="p-4">
+      <p className="font-nunito text-2xl font-bold">Upcoming Schedule</p>
+    </div>
+  );
+};
+
+export default Calendar;
