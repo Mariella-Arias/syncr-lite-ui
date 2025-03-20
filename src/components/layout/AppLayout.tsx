6b221c19fff26a7857685976e8594ab3a12bc8ac
Mariella-Arias <ariasmariella.brm@gmail.com>
2025-03-19 19:54:28 -0500

    Import modals, modal context provider, and initialize data in redux

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,19 +1,74 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router';
+import { useDispatch } from 'react-redux';
 
 import Navbar from './Navbar';
-import { ModalProvider } from '../../context/ModalProvider';
-import DeleteAccountModal from '../../features/auth/components/DeleteAccountModal';
+import { ModalsProvider } from '../../context/ModalsProvider';
+import SlideInModal from '../overlays/SlideInModal';
+import CenteredModal from '../overlays/CenteredModal';
+import {
+  setWorkouts,
+  setExercises,
+} from '../../features/workouts/workoutsSlice';
+import { AppDispatch } from '../../app/store';
 
 const AppLayout = () => {
+  const dispatch: AppDispatch = useDispatch();
+
+  useEffect(() => {
+    const initializeData = async () => {
+      console.log('Initializing data');
+      try {
+        await dispatch(setWorkouts());
+        await dispatch(setExercises());
+      } catch (error) {
+        console.error('Failed to initialize data', error);
+      }
+    };
+
+    initializeData();
+  }, []);
+
   return (
-    <ModalProvider>
-      <div className="w-screen h-screen">
-        <DeleteAccountModal />
+    <ModalsProvider>
+      <SlideInModal />
+      <CenteredModal />
+
+      <div className="w-screen h-screen flex flex-col">
         <Navbar />
         <Outlet />
       </div>
-    </ModalProvider>
+    </ModalsProvider>
   );
 };
 
 export default AppLayout;
+
+// Desktop grid layout
+{
+  /* <div className="absolute inset-0 pointer-events-none">
+  <div className="grid grid-cols-12 gap-5 h-full">
+    {[...Array(12)].map((_, index) => (
+      <div
+        key={index}
+        className="h-full bg-red-500 border-x border-red-500 opacity-8"
+      />
+    ))}
+  </div>
+</div> */
+}
+
+// Mobile grid layout
+{
+  // MOBILE LAYOUT GRID
+  /* <div className="fixed inset-0 w-screen h-screen pointer-events-none">
+<div className="grid grid-cols-5 gap-5 h-full">
+{[...Array(5)].map((_, index) => (
+  <div
+    key={index}
+    className="h-full bg-red-500 border-x border-red-500 opacity-8"
+  />
+))}
+</div>
+</div> */
+}
