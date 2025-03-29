14a6f6db40a80004a9f2f114030839cbec6faca4
mariella-arias <arias.mariella1@gmail.com>
2025-03-29 17:36:03 -0500

    Setup calendar redux slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import AuthReducer from '../features/auth/authSlice';
 import WorkoutsReducer from '../features/workouts/workoutsSlice';
+import CalendarReducer from '../features/calendar/calendarSlice';
 
 export const store = configureStore({
   reducer: {
     auth: AuthReducer,
     workouts: WorkoutsReducer,
+    calendar: CalendarReducer,
   },
 });
 
---
+++ src/features/calendar/calendarSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { RootState } from '../../app/store';
+import { IActivityEntry } from '../activity/types/activity.types';
+
+interface IInitialState {
+  scheduledWorkouts: IActivityEntry[];
+}
+
+const initialState: IInitialState = {
+  scheduledWorkouts: [],
+};
+
+const calendarSlice = createSlice({
+  name: 'calendar',
+  initialState,
+  reducers: {
+    setScheduledWorkouts: (state, action) => {
+      state.scheduledWorkouts = action.payload;
+      return state;
+    },
+  },
+});
+
+export const calendar = (state: RootState) => state.calendar;
+
+export const { setScheduledWorkouts } = calendarSlice.actions;
+
+export default calendarSlice.reducer;
