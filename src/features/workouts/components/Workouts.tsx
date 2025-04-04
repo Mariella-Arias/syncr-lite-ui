0f87e51e8f305b943761e0c9150c79b7465553c1
mariella-arias <arias.mariella1@gmail.com>
2025-04-03 23:33:21 -0500

    Implement quick search in workouts component to filter workouts by name

--- src/features/workouts/components/Workouts.tsx
+++ src/features/workouts/components/Workouts.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { X } from 'lucide-react';
 
 import Button from '../../../components/common/Button';
 import { workouts as workoutList } from '../workoutsSlice';
@@ -10,16 +12,40 @@ const Workouts = () => {
   const { workouts } = useSelector(workoutList);
   const { open: openSlideIn } = useSlideInModalContext();
 
+  const [query, setQuery] = useState('');
+
+  const searchResults = workouts.filter((workout) =>
+    workout.name.toLowerCase().includes(query.toLowerCase())
+  );
+
   return (
     <div className="flex flex-col h-full p-4">
       {/* HEADER */}
       <div className="flex flex-col mb-4">
         <p className="font-nunito text-2xl font-bold mb-6">My Workouts</p>
         <div className="flex justify-between">
-          <input
-            placeholder="Search"
-            className="border-input-border border-1 rounded-full py-2 px-6 text-lg"
-          />
+          <div className="relative ">
+            <input
+              placeholder="Search"
+              className="border-input-border border-1 rounded-full py-2 px-6 text-lg"
+              value={query}
+              onChange={(e) => {
+                setQuery(e.target.value);
+              }}
+            />
+
+            {query.length > 0 && (
+              <button
+                type="button"
+                onClick={() => setQuery('')}
+              >
+                <X
+                  strokeWidth={1.5}
+                  className="absolute right-3 top-1/2 -translate-y-1/2 rounded-full cursor-pointer hover:stroke-2"
+                />
+              </button>
+            )}
+          </div>
 
           <Button
             onClick={() => openSlideIn(<CreateWorkoutModal />)}
@@ -33,7 +59,7 @@ const Workouts = () => {
       {/* Scrollable content */}
       <div className="flex-1 overflow-y-auto overflow-x-hidden">
         <div className="grid grid-cols-[repeat(auto-fill,minmax(220px,1fr))] gap-6 justify-items-center">
-          {workouts.map((workout) => (
+          {searchResults.map((workout) => (
             <WorkoutCard
               key={workout.id}
               {...workout}
