8cec26bd0e32003473d585740b02703cabf94a93
mariella-arias <arias.mariella1@gmail.com>
2025-04-28 19:07:12 -0500

    Collect auth types in one file

--- src/features/auth/types/auth.types.ts
+++ src/features/auth/types/auth.types.ts
@@ -2,3 +2,26 @@ export interface ILoginCredentials {
   email: string;
   password: string;
 }
+
+export interface IChangePasswordFormData {
+  current_password: string;
+  new_password: string;
+  re_new_password: string;
+}
+
+export interface IDeleteAccountFormData {
+  current_password: string;
+}
+
+export interface IResetPasswordFormData {
+  new_password: string;
+  re_new_password: string;
+}
+
+export interface IUserCreate {
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  re_password: string;
+}
