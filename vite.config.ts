be311248f73c626d606604911e6e267e1a2cc7f6
mariella-arias <arias.mariella1@gmail.com>
2025-06-03 11:53:15 -0500

    fix: update Vite config for Github Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    allowedHosts: true,
-  },
+  base: '/syncr-lite-ui/',
 });
